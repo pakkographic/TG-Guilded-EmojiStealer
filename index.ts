b61013df1a65328c081911bc861eb2d274dc5a35
pakkographic <wade@dinographicpixels.com>
2024-09-14 15:24:37 +0200

    TG v1.4.1

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 // Imports
 import { ExtendedClient } from "./structures/ExtendedClient";
 import { config as configDotEnv } from "dotenv";
+import { GatewayLayerIntent } from "touchguild";
 
 configDotEnv(); // configure .env file to make it usable
 
@@ -8,6 +9,8 @@ const client = new ExtendedClient({
     token:                process.env.TOKEN ?? "no .env token",
     // will be used for commands (e.g: /myapp command)
     applicationShortname: "stealer",
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    intents:              [GatewayLayerIntent.ALL],
     basePath:             __dirname
 });
 
